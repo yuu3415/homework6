221f9fa46ed55909f16e8eb115036c9524ddcbda
yuu3415 <buddyyou6@gmail.com>
2023-09-14 12:47:49 +0900

    アニメリストを追加もエラー

--- src/main/java/com/hello/hello/HelloController.java
+++ src/main/java/com/hello/hello/HelloController.java
@@ -3,11 +3,24 @@ package com.hello.hello;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello World";
+
+    public static void main(String[] args) {
+
+        List<Anime> animelist = List.of(
+                new Anime(1, "鬼滅の刃", "バトル"),
+                new Anime(2, "こち亀", "コメディ"),
+                new Anime(3, "ニセコイ", "恋愛"));
+
+        return animelist;
+
+        for (Anime anime : animelist) {
+            return "このアニメの名前は" + anime.getName() + "でジャンルは" + anime.getCategory() + "です。";
+        }
     }
 }
